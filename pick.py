a1c4b6647508c7685fc08353bd9d57be4eba9290
MountDumb <torb3755@edu.eal.dk>
2018-06-18 23:17:23 +0200

    Some Updates

---
+++ pick.py
@@ -0,0 +1,21 @@
+import pickle
+import matplotlib.pyplot as plt
+
+history = None
+with open('Model/BinaryClasshistoryDict', 'rb') as file:
+    history = pickle.load(file)
+
+loss = history['loss']
+val_loss = history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='val_Loss')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.Show()
+
